a84b7104733c36f355efe802e5bd70faee3598e8
Jason Hargrove <rijaha@gmail.com>
2021-11-12 09:02:11 -0600

    Broke UI design into components. Starting static version.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function SearchBar() {
   return (
-    <div className="App">
-      <h1>Groovy</h1>
-    </div>
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+      {' '}
+      Only show products in stock
+      </label>
+    </form>
   );
 }
 
-export default App;
+function FilterableProductTable() {
+  return (
+    <div>
+      <h1>Groovy!!!</h1>
+      <SearchBar />
+    </div>
+  )
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: true, name: "passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: true, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+  return <FilterableProductTable />
+}
+
+{/* Ended at step 2. Build a static version in React */}
