87b0c2beb4ec2f62153ad2940165bd4818c20ece
Jason Hargrove <rijaha@gmail.com>
2021-12-08 16:58:39 -0600

    Searchable product data table with React. Static version complete.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import './App.css';
 
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    )
+}
+
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>
+            {product.name}
+        </span>;
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    )
+}
+
 function ProductTable( { products }) {
     const rows = [];
     let lastCategory = null;
@@ -58,9 +82,9 @@ function FilterableProductTable( { products }) {
 const PRODUCTS = [
   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
   {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: true, name: "passionfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "passionfruit"},
   {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: true, name: "Pumpkin"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
   {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
 ];
 
